f97c30cba54cf82484700ed9d8139e92509e20f1
Max-MI115 <max.murphy11@gmail.com>
2022-09-30 17:26:33 -0400

    Add to Rawan example script

--- example_load_and_sync_data.m
+++ example_load_and_sync_data.m
@@ -1,2 +1,125 @@
 clear; 
 clc;
+
+SUBJ = "Rawan";
+YYYY = 2022;
+MM = 9;
+DD = 19;
+BLOCK = 2;
+ELIM = [20 80];
+
+x = io.load_tmsi(SUBJ, YYYY, MM, DD, ["A", "B"], BLOCK, '.poly5');
+
+% Get the onset difference (seconds)
+dt = seconds(datetime(x(2).date) - datetime(x(1).date));
+fs = x(1).sample_rate;
+
+% How many samples ahead of "A" (x(1)) is "B" (x(2))?
+n_sync_samples = round(fs*dt);
+
+% Remove the first `n_sync_samples` from "B".
+iStart = n_sync_samples+1;
+iEnd = min(size(x(2).samples,2)-n_sync_samples, size(x(1).samples, 2));
+xA = x(1).samples(1:64,1:iEnd);
+xB = x(2).samples(1:64, (n_sync_samples+1):(n_sync_samples+iEnd));
+
+
+fig = figure('Name', 'Sample RMS', 'Color', 'w', 'Position', [4014 -715  1317  420]);
+L = tiledlayout(fig, 1, 2);
+title(L, sprintf('%s\\_%04d\\_%02d\\_%02d\\_%d',SUBJ,YYYY,MM,DD,BLOCK), ...
+    'FontName','Tahoma','Color','k'); 
+ax = nexttile(L);
+set(ax,'NextPlot','add','XColor','k','YColor','k','FontName','Tahoma');
+title(ax,'A', 'FontName','Tahoma','Color',[0.3 0.3 0.3]);
+rA = rms(xA,2)./1e3;
+histogram(ax, rA);
+xlabel(ax, 'RMS (mV)', 'FontName','Tahoma');
+xline(ax, ELIM, 'r:', 'Label', 'Exclusions');
+xlim(ax,[0 100]);
+ax = nexttile(L);
+set(ax,'NextPlot','add','XColor','k','YColor','k','FontName','Tahoma');
+title(ax,'B', 'FontName','Tahoma','Color',[0.3 0.3 0.3]);
+rB = rms(xB,2)./1e3;
+histogram(ax, rB);
+xlabel(ax, 'RMS (mV)', 'FontName','Tahoma');
+xlim(ax,[0 100]);
+xline(ax, ELIM, 'r:', 'Label', 'Exclusions');
+default.savefig(fig, "R:/NMLShare/generated_data/human/TC/MUAPs/RMS");
+
+%%
+xA((rA < ELIM(1)) | (rA > ELIM(2)), :) = [];
+xB((rB < ELIM(1)) | (rB > ELIM(2)), :) = [];
+
+%%
+yA = xA - mean(xA,2);
+% yA = yA - mean(yA,1);
+
+yB = xB - mean(xB,2);
+% yB = yB - mean(yB,1);
+
+[b,a] = butter(4, ([25 400])./4000, 'bandpass');
+zB = filtfilt(b,a,yB')';
+zA = filtfilt(b,a,yA')';
+
+zB = zB - mean(zB,1);
+zA = zA - mean(zA,1);
+
+t = 0:(1/fs):((iEnd-1)/fs);
+Y = [zA; zB];
+
+%%
+[coeff,score,latent,tsquared,explained,mu] = pca(Y');
+fig = figure('Name', 'Pareto Plot', 'Color', 'w');
+ax = axes(fig, 'NextPlot', 'add');
+pareto(ax, explained);
+default.savefig(fig, "R:/NMLShare/generated_data/human/TC/MUAPs/Pareto");
+
+%% The first PC is just noise.
+fig = figure('Name', 'PC-1 Noise', 'Color', 'w');
+spectrogram(score(:,1), 512, 256, 0:500, 4000, 'yaxis');
+title("PC-1 is mains noise");
+xlabel("Time");
+ylabel("Amplitude (\muV)");
+default.savefig(fig, "R:/NMLShare/generated_data/human/TC/MUAPs/PC1-Noise");
+
+%% Look next at PC-2 thru PC-5
+fig = figure('Name', 'PC-2 thru PC-5', 'Color', 'w', 'Position', [4008        -996        1067         892]);
+L = tiledlayout(fig, 'flow');
+for ii = 2:5
+    ax = nexttile(L);
+    set(ax, 'NextPlot', 'add', 'FontName', 'Tahoma');
+    axes(ax);
+    spectrogram(score(:,ii), 512, 256, 0:500, 4000, 'yaxis');
+    
+    xlabel(ax, 'Time (mins)', 'FontName', 'Tahoma');
+    ylabel(ax, 'Frequency (Hz)', 'FontName', 'Tahoma');
+    ylim(ax, [0 300]);
+    if ii == 5
+        title(ax, sprintf('PC-%d (noise)', ii), 'FontName', 'Tahoma', 'Color', 'r');
+    else
+        title(ax, sprintf('PC-%d', ii), 'FontName', 'Tahoma', 'Color', 'k');
+    end
+end
+default.savefig(fig, "R:/NMLShare/generated_data/human/TC/MUAPs/PC2-5");
+
+%% Look next at PC-6 thru PC-11
+fig = figure('Name', 'PC-6 thru PC-11', 'Color', 'w', 'Position', [4008        -996        1067         892]);
+L = tiledlayout(fig, 'flow');
+for ii = 6:11
+    ax = nexttile(L);
+    set(ax, 'NextPlot', 'add', 'FontName', 'Tahoma');
+    axes(ax);
+    spectrogram(score(:,ii), 512, 256, 0:500, 4000, 'yaxis');
+    
+    xlabel(ax, 'Time (mins)', 'FontName', 'Tahoma');
+    ylabel(ax, 'Frequency (Hz)', 'FontName', 'Tahoma');
+    ylim(ax, [0 300]);
+    if ii == 5
+        title(ax, sprintf('PC-%d (noise)', ii), 'FontName', 'Tahoma', 'Color', 'r');
+    else
+        title(ax, sprintf('PC-%d', ii), 'FontName', 'Tahoma', 'Color', 'k');
+    end
+end
+default.savefig(fig, "R:/NMLShare/generated_data/human/TC/MUAPs/PC6-11");
+
+
--- parameters.m
+++ parameters.m
@@ -2,10 +2,10 @@ function varargout = parameters(varargin)
 %PARAMETERS Return parameters struct, which sets default values for things like epoch durations etc.
 %
 % Example 1
-%   pars = parameters(); % Returns full "default parameters" struct.
+%   pars = utils.parameters(); % Returns full "default parameters" struct.
 %
 % Example 2
-%   f = parameters('raw_data_folder'); % Returns raw data folder value
+%   f = utils.parameters('raw_data_folder'); % Returns raw data folder value
 %
 % I tried to pull the key parameters, which are really path information
 % from your local mapping to where the raw data is and where any
@@ -16,10 +16,28 @@ function varargout = parameters(varargin)
 % See also: Contents, load_tmsi_raw
 
 pars = struct;
+
+% % % Trying to pull the "relevant" ones to the top ... % % % 
 pars.data_share_root = 'R:/NMLShare';
 pars.raw_data_folder = fullfile(pars.data_share_root, 'raw_data/primate');
 pars.generated_data_folder = fullfile(pars.data_share_root, 'generated_data/human/TC');
-pars.task_bits_file = "wrist_task_bits.json";pars.version = 1.0; % "Version" of repo code -- check that this matches the TAG
+pars.animations_folder = fullfile(pars.data_share_root, 'generated_data/primate/Animations');
+pars.preliminary_output_folder = fullfile(pars.generated_data_folder, '.preliminary'); % . prefix defaults to "Hidden" on Windows
+pars.channels_spreadsheet_expr = "%s_CHANNELS.xlsx";
+pars.events_spreadsheet_expr = "%s_EVENTS.xlsx";
+pars.raw_matfiles_folder = ".raw_channels";
+pars.raw_matfiles_expr = "%s_RAW_%%d.mat";
+pars.events_file_expr = "%s_EVENTS.mat";
+pars.meta_file_expr = "%s_META.mat";
+pars.alignment_parent_folder = ".aligned";
+pars.alignment_folder = struct( ...
+    'MOVE_ONSET', ".move", ...
+    'MOVE_THRESHOLD', ".move_thresh", ...
+    'T1_HOLD_ONSET', ".t1_hit", ...
+    'T2_HOLD_ONSET', ".t2_hit", ...
+    'OVERSHOOT_ONSET', ".overshoot", ...
+    'REWARD_ONSET', ".reward");
+pars.version = 1.0; % "Version" of repo code -- check that this matches the TAG
 
 N = numel(varargin);
 if nargout == 1
